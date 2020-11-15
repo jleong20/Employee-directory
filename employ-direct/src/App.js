e10c766d2443992082752ee9227d4c2e4fe196bc
Jerry Leong <jerry.leong20@gmail.com>
2020-11-14 23:12:37 -0800

    add employeeList component

--- employ-direct/src/App.js
+++ employ-direct/src/App.js
@@ -4,18 +4,31 @@ import EmployeeJSON from "./empList.json";
 
 class App extends Component{
   render(){
+    const styles={
+      card:{
+        background: "#BEBEBE",
+      }
+    }
     return(
       <div class="container">
         <h1>Employee Directory</h1>
         <input type="text" placeholder="Search.."></input>
-        <div>
-          <div class="card-header">Employee Directory</div>
+    
+        <div class="card" style={styles.card}>
+          <div class="card-header">
+            <ul class="nav column">
+              <li class="nav-item col-sm-4">Name</li>
+              <li class="nav-item col-sm-4">Email</li>
+              <li class="nav-item col-sm-4">Phone</li>
+            </ul>
+          </div>
           <ul class="list-group list-group-flush">
             {EmployeeJSON.map(Employee =>(
-            <EmployeeList id={EmployeeJSON.id} name={EmployeeJSON.name} />
+            <EmployeeList id={Employee.id} name={Employee.name} email={Employee.email} phone={Employee.phone}/>
             ))}
           </ul>
-        </div>
+          
+        </div> 
       </div>
     )
   }
---
+++ employ-direct/src/components/empList/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function EmployeeList(props){
+    return(
+        <li class="list-group-item">
+            <ul class="nav column">
+                <li class="nav-item col-sm-4">Name: {props.name}</li>
+                <li class="nav-item col-sm-4">Email: {props.email}</li>
+                <li class="nav-item col-sm-4">Phone: {props.phone}</li>
+              </ul>
+        </li>
+    );
+}
+
+export default EmployeeList;
